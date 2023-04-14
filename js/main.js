6026a703a7e626dd314c59c36860b8c9ec0b4ca2
fvandam <302683815@student.rocmondriaan.nl>
2023-04-14 21:21:22 +0200

    queryselected btn

--- js/main.js
+++ js/main.js
@@ -1,2 +1,9 @@
+//queryselectors
 const indexDutch = document.querySelector('.dutch-version');
 const indexEnglish = document.querySelector('.english-version');
+const translateDutch = document.querySelector('.translate-dutch');
+
+//translate toggle function
+
+
+//event listener
