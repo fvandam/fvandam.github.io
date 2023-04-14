e4aa445700473dada6a3963497c709dddae048ae
fvandam <302683815@student.rocmondriaan.nl>
2023-04-14 21:47:57 +0200

    function + eventlistener

--- js/main.js
+++ js/main.js
@@ -1,9 +1,16 @@
 //queryselectors
 const indexDutch = document.querySelector('.dutch-version');
 const indexEnglish = document.querySelector('.english-version');
-const translateDutch = document.querySelector('.translate-dutch');
+const dutchBtn = document.querySelector('.translate-dutch');
+const englishBtn = document.querySelector('.translate-english');
 
 //translate toggle function
 
+function translateDutch(){
+    indexDutch.classList.toggle('hidden');
+    indexEnglish.classList.toggle('hidden');
+}
 
 //event listener
+dutchBtn.addEventListener('click', translateDutch);
+englishBtn.addEventListener('click', translateDutch);
