c6b04db095a8134135e96e200a68a53fee8cf984
fvandam <302683815@student.rocmondriaan.nl>
2023-04-14 22:07:46 +0200

    queryselected for about

--- js/main.js
+++ js/main.js
@@ -4,13 +4,21 @@ const indexEnglish = document.querySelector('.english-version');
 const dutchBtn = document.querySelector('.translate-dutch');
 const englishBtn = document.querySelector('.translate-english');
 
+const aboutEnglish = document.querySelector('.about-english');
+const aboutDutch = document.querySelector('.about-dutch');
+const translateAboutE = document.querySelector('.translate-aboutE');
+const translateAboutD = document.querySelector('.translate-aboutD');
+
 //translate toggle function
 
-function translateDutch(){
+function translateDutchIndex(){
     indexDutch.classList.toggle('hidden');
     indexEnglish.classList.toggle('hidden');
+
+    aboutEnglish.classList.toggle('hidden');
+    aboutDutch.classList.toggle('hidden');
 }
 
 //event listener
-dutchBtn.addEventListener('click', translateDutch);
-englishBtn.addEventListener('click', translateDutch);
+dutchBtn.addEventListener('click', translateDutchIndex);
+englishBtn.addEventListener('click', translateDutchIndex);
