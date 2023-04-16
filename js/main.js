822495f58f7730421058b7037bb1feef54c598b5
fvandam <302683815@student.rocmondriaan.nl>
2023-04-16 19:41:52 +0200

    queryselected

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
+const homeBlock = document.querySelector('.home-block');
+const aboutBlock = document.querySelector('.about-block');
+const workBlock = document.querySelector('.work-block');
+const projectBlock = document.querySelector('.project-block');
+const contactBlock = document.querySelector('.contact-block');
 
+let page = homeBlock;
+
+function translatePage(){
+
+}
